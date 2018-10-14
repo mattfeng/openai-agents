8306e28a8c35f7270d15a41353fbed6611767822
Matthew Feng <mattfeng@mit.edu>
2018-10-14 12:35:59 -0400

    Display

--- breakout/main.py
+++ breakout/main.py
@@ -2,24 +2,44 @@
 
 import torch as T
 import torch.nn as nn
+import torchvision.transforms as V
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 import sys
 sys.path.append("/Users/mattfeng/torchutils/")
 from torchutils.bootstrap import bootstrap
+from torchutils.viz.display import Display
+
+DISPLAY_WIDTH = 600
+DISPLAY_HEIGHT = 600
 
 @bootstrap.main
 def main(*args, **kwargs):
     M = kwargs["M"]
+    M.display = Display(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+
     env = gym.make("BreakoutDeterministic-v4")
     frame = env.reset()
     env.render()
 
     is_done = False
+    transform = V.Compose([
+        V.ToPILImage(),
+        V.Resize((160, 160)),
+        V.ToTensor()
+    ])
+
     while not is_done:
         frame, reward, is_done, _ = env.step(env.action_space.sample())
+        frame = transform(frame)
+
+        M.display.draw_torchvision(frame, 0, 0)
+
         env.render()
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
--- breakout/model.py
+++ breakout/model.py
@@ -1,9 +1,33 @@
 import torch as T
 import torch.nn as nn
+import torch.functional
 
-def ReplayMemory(object):
-    pass
+class DQN(nn.Module):
+    """
+    DQN (Deep Q-Network)
+
+    """
+
+    def __init__(self):
+        super(DQN, self).__init__()
+        # input size (210, 160, 3)
+        self.conv1 = nn.Conv2d(3, 32)
+        self.bn1 = nn.BatchNorm2d(32) # num_features = # channels
+        self.conv2 = nn.Conv2d(32, 64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear()
+        self.fc2 = nn.Linear()
+
+    def forward(self, x):
+        """
+
+        Parameters
+        ----------
+        x :
 
 
-def DQN(nn.Module):
-    pass
+        """
+        pass
+        
