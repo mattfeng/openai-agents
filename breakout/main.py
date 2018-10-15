70ace980609fd4b9fe2412ac4806d8417447301d
Matthew Feng <mattfeng@mit.edu>
2018-10-15 15:58:02 -0400

    NOP is bad

--- breakout/main.py
+++ breakout/main.py
@@ -128,12 +128,15 @@ def train(M):
 
         same = T.all(T.lt(
             T.abs(T.add(prev_frame, -frame)), 1e-8)).item()
+        
+        if action == 0:
+            print('bad')
+            reward -= 1.0
 
         if same == 0:
             consecutive_same = 0
         else:
             consecutive_same += 1
-            reward -= 10
 
         if consecutive_same > 40:
             done = True
