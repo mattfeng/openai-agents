6a74cf1861aa0007735affbfdbb2d0fe11b085ff
Matthew Feng <mattfeng@mit.edu>
2018-10-25 06:45:11 -0400

    Use DDQN

--- enduro/main.py
+++ enduro/main.py
@@ -206,14 +206,14 @@ def optimize(M):
     # Get the values of Q(s, a) for the actions that we actually took
     state_action_values = M.policy(state_batch).gather(1, action_batch.view(-1, 1))
 
-    next_state_values = T.zeros(BATCH_SIZE, device=M.device)
-    next_state_values[non_final_mask] = \
-        M.target(non_final_next_states).max(dim=1)[0].detach()
+    # next_state_values = T.zeros(BATCH_SIZE, device=M.device)
+    # next_state_values[non_final_mask] = \
+    #     M.target(non_final_next_states).max(dim=1)[0].detach()
 
     # Implement Double Q-Learning
-    # est_best_actions = M.target(non_final_next_states).argmax(dim=1).detach().view(-1, 1)
-    # next_state_values[non_final_mask] = M.policy(
-    #     non_final_next_states).detach().gather(1, est_best_actions).squeeze()
+    est_best_actions = M.policy(non_final_next_states).argmax(dim=1).detach().view(-1, 1)
+    next_state_values[non_final_mask] = M.target(
+        non_final_next_states).detach().gather(1, est_best_actions).squeeze()
 
     # Update only those Q(s, a) where we actually took action a
     expected_state_action_values = reward_batch + (GAMMA * next_state_values)
