7b69a3543b615c6e47d27d9c4cdad3e09d96ad7c
Matthew Feng <mattfeng@mit.edu>
2018-10-25 10:59:33 -0400

    Try space invaders

---
+++ spaceinvaders/model.py
@@ -0,0 +1,75 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class DuelingDQN(nn.Module):
+    def __init__(self, in_channels, num_moves):
+        super(DuelingDQN, self).__init__()
+        self.num_moves = num_moves
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+
+        self.adv_fc1 = nn.Linear(7 * 7 * 64, 512)
+        self.adv_fc2 = nn.Linear(512, num_moves)
+
+        self.val_fc1 = nn.Linear(7 * 7 * 64, 512)
+        self.val_fc2 = nn.Linear(512, 1)
+    
+    def forward(self, x):
+        x = x.view(-1, 2, 84, 84)
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        x = x.view(-1, 7 * 7 * 64)
+
+        adv = F.relu(self.adv_fc1(x))
+        adv = F.relu(self.adv_fc2(adv))
+
+        val = F.relu(self.val_fc1(x))
+        val = F.relu(self.val_fc2(val)).expand(-1, self.num_moves)
+
+        # print("adv", adv - adv.mean(1, keepdim=True).expand(-1, self.num_moves))
+        # print("val", val)
+
+        x = val + (adv - adv.mean(1, keepdim=True).expand(-1, self.num_moves))
+
+        return x
+
+class DQN(nn.Module):
+    """
+    DQN (Deep Q-Network)
+    """
+
+    def __init__(self, in_channels, num_moves):
+        super(DQN, self).__init__()
+        # input size (84, 84, 2)
+        # Takes the current and previous frames
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(7 * 7 * 64, 512)
+        self.fc2 = nn.Linear(512, num_moves)
+
+    def forward(self, x):
+        """
+        Parameters
+        ----------
+        x :
+        """
+        x = x.view(-1, 2, 84, 84)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc1(x.view(-1, 7 * 7 * 64)))
+        x = F.relu(self.fc2(x))
+
+        return x
+
+if __name__ == "__main__":
+    net = DuelingDQN(2, 9)
+    i = T.from_numpy(np.zeros(84 * 84 * 2).reshape((2, 84, 84))).float()
+    print(net(i))
