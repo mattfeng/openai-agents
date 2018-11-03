33d37d945db4b06d65ba6d1d4e7c4c9b700ba5fd
Matthew Feng <mattfeng@mit.edu>
2018-11-02 23:16:20 -0400

    Add normaization

--- cartpole/main.py
+++ cartpole/main.py
@@ -4,19 +4,24 @@ GAMMA = 0.99
 NUM_EPISODES = 5000
 MAX_STEPS = 999
 
-def discounted_returns(rewards):
+def discounted_returns(rewards, normalize=True):
     """
     Args:
         rewards (np.array): Array of rewards at each timestep.
+        normalize (bool): Should the returns be normalized?
     Returns:
-        Array of discounted returns `G` (sum of rewards).
+        Array of discounted returns `G` (sum over discounted rewards).
     """
     discounted_g = np.zeros_like(rewards)
     cumsum = 0
     for t in range(rewards.size - 1, -1, -1):
         cumsum = cumsum * GAMMA + rewards[t]
         discounted_g[t] = cumsum
-    return dicounted_g
+    
+    mean = np.mean(discounted_g)
+    stdev = np.stdev(discounted_g)
+
+    return (discounted_g - mean) / stdev
 
 
 def test():
