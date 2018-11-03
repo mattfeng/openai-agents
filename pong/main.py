79b8ea94c4af5d9012aaddd6b13cccdf73e2696e
Matthew Feng <mattfeng@mit.edu>
2018-11-03 17:08:15 -0400

    Add pong REINFORCE

---
+++ pong/main.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+import gym
+import openai_envs
+
+import tensorflow as tf
+from tfutils.viz.display import Display
+from tfutils.env import Environment
+from tfutils.bootstrap import init_tensorboard
+from tfutils.preprocess import rgb_to_gray
+
+from model import Agent
+
+import numpy as np
+
+import os
+
+DISPLAY_WIDTH = 600
+DISPLAY_HEIGHT = 600
+DISP = os.environ["DISP"] == "y"
+
+NUM_EPISODES = 10000
+GAMMA = 0.99
+OPTIMIZER_OPTIONS = {
+    "learning_rate": 0.01
+}
+
+def discounted_returns(rewards, normalize=True):
+    """
+    Args:
+        rewards (np.array): Array of rewards at each timestep.
+        normalize (bool): Should the returns be normalized?
+    Returns:
+        Array of discounted returns `G` (sum over discounted rewards).
+    """
+    discounted_g = np.zeros_like(rewards)
+    cumsum = 0
+    for t in range(len(rewards) - 1, -1, -1):
+        cumsum = cumsum * GAMMA + rewards[t]
+        discounted_g[t] = cumsum
+    
+    mean = np.mean(discounted_g)
+    stdev = np.std(discounted_g)
+
+    if normalize:
+        return (discounted_g - mean) / stdev
+    return discounted_g
+
+def test(M):
+    pass
+
+def train(M):
+    state = M.env.reset()
+
+    states = []
+    actions = []
+    rewards = []
+
+    while True:
+        # Preprocess state
+        state = rgb_to_gray(state)
+        policy = M.sess.run(M.agent.policy, feed_dict={
+            M.agent.states: state.reshape([-1, 210, 160, 1])
+        })
+        policy = policy.squeeze() # policy.shape: (1, 6) -> (6, )
+
+        action = np.random.choice(
+            np.arange(M.env.action_space.n),
+            p=policy)
+
+        next_state, reward, done, _ = M.env.step(action)
+
+        states.append(state)
+        rewards.append(reward)
+
+        action_onehot = np.zeros_like(policy)
+        action_onehot[action] = 1
+
+        actions.append(action_onehot)
+
+        if DISP:
+            M.display.draw_vector(state, 0, 0, scale=2)
+
+        if done:
+            episode_return = np.sum(rewards)
+            M.total_return += episode_return
+
+            neg_obj, _ = M.sess.run([M.agent.neg_obj, M.agent.train_op],
+                feed_dict={
+                    M.agent.states: np.array(states),
+                    M.agent.actions: np.array(actions),
+                    M.agent.discounted_returns: discounted_returns(rewards)
+                })
+
+            summary = M.sess.run(M.write_op, feed_dict={
+                M.agent.neg_obj: neg_obj,
+                M.agent.mean_return: M.total_return / (M.ep + 1)
+            })
+
+            M.writer.add_summary(summary)
+            M.writer.flush()
+
+            return episode_return, neg_obj
+        
+        state = next_state
+    
+
+def main():
+    M = Environment()
+    M.env = gym.make("Pong-v0")
+    M.agent = Agent(M, OPTIMIZER_OPTIONS)
+
+    M.writer = init_tensorboard()
+    tf.summary.scalar("Negative J(theta)", M.agent.neg_obj)
+    tf.summary.scalar("Mean Return", M.agent.mean_return)
+    M.write_op = tf.summary.merge_all()
+
+    if DISP:
+        M.display = Display("pong", DISPLAY_WIDTH, DISPLAY_HEIGHT)
+
+    # Print basic info about the environment
+    print("Action set: {}".format(M.env.unwrapped._action_set))
+
+    M.total_return = 0
+
+    with tf.Session() as sess:
+        M.sess = sess
+        M.sess.run(tf.global_variables_initializer())
+
+        for ep in range(NUM_EPISODES):
+            M.ep = ep
+            episode_return, neg_obj = train(M)
+            print("[ep/{}] G: {:5.2f} -J(theta): {:0.12f}".format(
+                M.ep, episode_return, neg_obj))
+
+    
+if __name__ == "__main__":
+    main()
---
+++ pong/model.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+from tfutils.funcs import *
+
+class Agent(object):
+    def __init__(self, M, optimizer_options):
+        """
+        Agent using REINFORCE algorithm.
+        optimizer_options (dict): Options for the optimizer.
+        """
+        self.M = M
+
+        conv2d = tf.layers.conv2d
+        Linear = tf.contrib.layers.fully_connected
+        xntropy = tf.nn.softmax_cross_entropy_with_logits_v2
+
+        with tf.name_scope("informative"):
+            self.mean_return = tf.placeholder(
+                tf.float32,
+                name="mean_return")
+
+        with tf.name_scope("inputs"):
+            self.states = tf.placeholder(
+                tf.float32,
+                [None, 210, 160, 1],
+                "states")
+            self.actions = tf.placeholder(
+                tf.int32,
+                [None, self.M.env.action_space.n],
+                "actions")
+            self.discounted_returns = tf.placeholder(
+                tf.float32,
+                [None,],
+                "discounted_returns")
+            
+
+        with tf.name_scope("model"):
+            with tf.name_scope("conv1"):
+                self.conv1 = conv2d(
+                    self.states,
+                    filters=16,
+                    kernel_size=8,
+                    padding="same",
+                    strides=4
+                )
+
+            with tf.name_scope("conv2"):
+                self.conv2 = conv2d(
+                    self.conv1,
+                    filters=16,
+                    kernel_size=4,
+                    padding="same",
+                    strides=4
+                )
+
+            with tf.name_scope("conv3"):
+                self.conv3 = conv2d(
+                    self.conv2,
+                    filters=8,
+                    kernel_size=3,
+                    padding="same",
+                    strides=1
+                )
+
+            with tf.name_scope("fc1"):
+                self.conv_features = self.conv3.reshape([-1, 1120])
+                self.fc1 = Linear(
+                    self.conv_features,
+                    num_outputs=32
+                )
+
+            with tf.name_scope("fc2"):
+                self.fc2 = Linear(
+                    self.fc1,
+                    num_outputs=self.M.env.action_space.n
+                )
+            
+            with tf.name_scope("softmax"):
+                self.policy = tf.nn.softmax(logits=self.fc2)
+
+        with tf.name_scope("objective"):
+            nll = xntropy(
+                logits=self.fc2,
+                labels=self.actions
+            )
+            self.neg_obj = tf.reduce_mean(nll * self.discounted_returns)
+
+        with tf.name_scope("optimizer"):
+            self.train_op = tf.train.AdagradOptimizer(
+                **optimizer_options).minimize(self.neg_obj)
+
+
+if __name__ == "__main__":
+    import gym
+    from tfutils.env import Environment
+    from tfutils.preprocess import rgb_to_gray
+
+    M = Environment()
+    M.env = gym.make("Pong-v0")
+    state = M.env.reset()
+    state = rgb_to_gray(state)
+    print(state.shape)
+
+    M.agent = Agent(M, {
+        "learning_rate": 0.01
+    })
+
+    with tf.Session() as sess:
+        M.sess = sess
+        M.sess.run(tf.global_variables_initializer())
+        conv3 = M.sess.run(M.agent.conv3, feed_dict={
+            M.agent.states: state.reshape([-1, 210, 160, 1])
+        })
+        print(conv3.shape)
